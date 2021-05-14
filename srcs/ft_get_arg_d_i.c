d7925394c973f7bd921ae9603e3325a8e07d04a8
jodufour <jodufour@student.42.fr>
2021-05-14 05:59:57 +0200

    We apply flags, field width and precision well for d and i specifier

--- srcs/ft_get_arg_d_i.c
+++ srcs/ft_get_arg_d_i.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 04:38:42 by jodufour          #+#    #+#             */
-/*   Updated: 2021/05/14 04:23:50 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/05/14 05:57:56 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ static int	ft_count_digits(int n)
 	return (count);
 }
 
-static char	*ft_precision_itoa(int n, int padlen)
+static char	*ft_padded_itoa(int n, int c, int padlen)
 {
 	uint32_t	abs;
 	char		*output;
@@ -53,7 +53,34 @@ static char	*ft_precision_itoa(int n, int padlen)
 		abs /= 10;
 	}
 	while (len--)
-		*p-- = '0';
+		*p-- = c;
+	return (output);
+}
+
+static char	*ft_right_padding(char *output, char *dent, int field_width)
+{
+	int		padlen;
+	char	*padding;
+	char	*p;
+
+	padlen = field_width - (int)ft_strlen(dent);
+	if (padlen > 0)
+	{
+		padding = malloc((padlen + 1) * sizeof(char));
+		if (!padding)
+		{
+			free(output);
+			return (NULL);
+		}
+		p = padding;
+		while (padlen--)
+			*p++ = ' ';
+		*p = 0;
+		p = output;
+		output = ft_strjoin(output, padding);
+		free(p);
+		free(padding);
+	}
 	return (output);
 }
 
@@ -67,14 +94,17 @@ char	*ft_get_arg_d_i(t_ctx *ctx, va_list va)
 	n = va_arg(va, int);
 	digits = ft_count_digits(n);
 	if (ctx->precision > digits)
-	{
-		dent = ft_precision_itoa(n, ctx->precision - digits);
-	}
+		dent = ft_padded_itoa(n, '0', ctx->precision - digits);
+	else if ((ctx->flags & (1 << 1)) && !(ctx->flags & (1 << 0))
+		&& ctx->field_width > (digits + (n < 0)))
+		dent = ft_padded_itoa(n, '0', ctx->field_width - digits - (n < 0));
 	else
 		dent = ft_itoa(n);
 	if (!dent)
 		return (NULL);
 	output = ft_strjoin(ctx->print, dent);
+	if (ctx->flags & (1 << 0))
+		output = ft_right_padding(output, dent, ctx->field_width);
 	free(dent);
 	return (output);
 }
--- tests/srcs/main.c
+++ tests/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 01:31:25 by jodufour          #+#    #+#             */
-/*   Updated: 2021/05/14 04:37:31 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/05/14 05:58:49 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int	main(void)
 	int	ft_ret;
 	int	ret;
 
-	ret = printf("%.*d|\n", 8, -2147483647);
-	ft_ret = ft_printf("%.*d|\n", 8, -2147483647);
+	ret = printf("%-*.*d|\n", 4, 2, -7);
+	ft_ret = ft_printf("%-*.*d|\n", 4, 2, -7);
 	printf("ft_ret -> %d\n", ft_ret);
 	printf("   ret -> %d\n", ret);
 	return (SUCCESS);
