72bea5cd3efb5c8e4777ad310acbd749ee69de7c
jodufour <jodufour@student.42.fr>
2021-05-19 01:41:10 +0200

    Stable version for all the mandatory, adding '+', ' ', '#' flags, but not working well yet

--- srcs/get_arg_d_lower.c
+++ srcs/get_arg_d_lower.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 06:19:49 by jodufour          #+#    #+#             */
-/*   Updated: 2021/05/19 00:45:31 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/05/19 01:35:40 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,13 @@ static int	padded_putnbr(int n, uint32_t len, t_ctx *ctx)
 		write(1, " ", 1);
 	if (ctx->flags & (1 << 1) && padding('0', padlen) == MALLOC_ERRNO)
 		return (MALLOC_ERRNO);
-	padlen = ctx->prec - (len - ((n < 0) || !!(ctx->flags & (1 << 2)) || !!(ctx->flags & (1 << 3))));
+	padlen = ctx->prec - (len - !!((n < 0) || (ctx->flags & (1 << 2)) || (ctx->flags & (1 << 3))));
 	if (padlen && padding('0', padlen) == MALLOC_ERRNO)
 		return (MALLOC_ERRNO);
 	ft_putunbr((n < 0) * (-n) + (n >= 0) * n);
 	if (ctx->flags & (1 << 0))
 	{
-		padlen = ctx->fwidth - ctx->prec - (n < 0);
+		padlen = ctx->fwidth - ctx->prec - !!((n < 0) || (ctx->flags & (1 << 2)) || (ctx->flags & (1 << 2)));
 		if (padding(' ', padlen) == MALLOC_ERRNO)
 			return (MALLOC_ERRNO);
 	}
@@ -60,8 +60,8 @@ int	get_arg_d_lower(t_ctx *ctx, va_list va)
 	len = ft_intlen(n);
 	if (ctx->prec < (len - (n < 0)))
 		ctx->prec = len - (n < 0);
-	if (ctx->fwidth < (ctx->prec + ((n < 0) || !!(ctx->flags & (1 << 2)) || !!(ctx->flags & (1 << 3)))))
-		ctx->fwidth = ctx->prec + ((n < 0) || !!(ctx->flags & (1 << 2)) || !!(ctx->flags & (1 << 3)));
+	if (ctx->fwidth < (ctx->prec + !!((n < 0) || (ctx->flags & (1 << 2)) || (ctx->flags & (1 << 3)))))
+		ctx->fwidth = ctx->prec + !!((n < 0) || (ctx->flags & (1 << 2)) || (ctx->flags & (1 << 3)));
 	ctx->len += ctx->fwidth;
 	if (ctx->fwidth > len)
 		return (padded_putnbr(n, len, ctx));
--- tests/srcs/main.c
+++ tests/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 01:31:25 by jodufour          #+#    #+#             */
-/*   Updated: 2021/05/19 00:36:07 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/05/19 01:39:35 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 #include <stdlib.h>
 #include "ft_printf.h"
 
-#define TEST "|%#+*.*x|\n", -30, 20, 2147483647
+#define TEST "|%#0*.*x|\n", 5, -1, 1
 
 int	main(void)
 {
 	int	ret;
 	int	ft_ret;
 
+	ft_ret = ft_printf(TEST);
 	ret = printf(TEST);
 	fflush(stdout);
-	ft_ret = ft_printf(TEST);
 	printf("ft_ret -> %d\n", ft_ret);
 	fflush(stdout);
 	printf("   ret -> %d\n", ret);
